7fb6dcda3e1759c934f42ec79af4d494310fd02e
mounikavelidindi <mounikavelidindi044@gmail.com>
2026-06-11 14:48:52 +0530

    Personal Portfolio

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.querySelectorAll('nav a').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
     e.preventDefault();
-    const section = document.querySelector(this.getAttribute('href'));
+    const section = document.querySelector(this.getAttribute('href'));  
     section.scrollIntoView({ behavior: 'smooth' });
   });
 });
